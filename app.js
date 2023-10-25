ac20ce273c85d297913800f864b25f703aa1c36c
kimwoojin123 <kimwoojin1903@gmail.com>
2023-10-25 10:06:13 +0900

    서버 오류 수정

--- app.js
+++ app.js
@@ -3,51 +3,47 @@ const fs = require("fs");
 const path = require("path");
 
 const server = http.createServer((req, res) => {
-  // URL을 기반으로 요청 경로를 추출
-  const url = req.url;
+  let filePath = path.join(__dirname, req.url);
 
-  // '/' 경로로 들어온 요청을 index.html로 매핑
-  if (url === "/") {
-    const filePath = "./index.html";
-    fs.readFile(filePath, (error, content) => {
-      if (error) {
+  // 확인할 파일 경로가 디렉토리인 경우 index.html을 사용
+  if (fs.statSync(filePath).isDirectory()) {
+    filePath = path.join(filePath, "index.html");
+  }
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === "ENOENT") {
+        // 파일이 없을 때 404 응답
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("Not Found");
+      } else {
+        // 다른 오류에 대한 500 응답
         res.writeHead(500);
         res.end("Internal Server Error");
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content, "utf-8");
       }
-    });
-  } else {
-    // 다른 경로로 들어온 요청에 대한 처리를 추가 (서브페이지)
-    // 예를 들어, '/subpage1'에 해당하는 서브페이지가 있다고 가정
-    if (url === "/subpage1") {
-      const filePath = "./subpages/subpage1.html";
-      fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (!err) {
-          fs.readFile(filePath, (error, content) => {
-            if (error) {
-              res.writeHead(500);
-              res.end("Internal Server Error");
-            } else {
-              res.writeHead(200, { "Content-Type": "text/html" });
-              res.end(content, "utf-8");
-            }
-          });
-        } else {
-          res.writeHead(404);
-          res.end("Not Found");
-        }
-      });
-    } else if (url === "/subpage2") {
-      // '/subpage2'에 해당하는 서브페이지 처리 추가
     } else {
-      // 다른 경로에 대한 처리를 추가할 수 있음
-      res.writeHead(404);
-      res.end("Not Found");
+      const contentType = getContentType(filePath);
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
     }
-  }
+  });
 });
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case ".html":
+      return "text/html";
+    case ".css":
+      return "text/css";
+    case ".js":
+      return "application/javascript";
+    case ".jpg":
+      return "image/jpg";
+    default:
+      return "text/plain";
+  }
+}
 
 const port = 3000;
 server.listen(port, () => {
